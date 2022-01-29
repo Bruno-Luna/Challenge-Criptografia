0630853951ce91aa410adf9602f430aa2ea9b132
Bruno <luna.brsilva@gmail.com>
2022-01-29 20:04:30 -0300

    LocalStorage e mediaScreen

--- js/script.js
+++ js/script.js
@@ -5,12 +5,17 @@
 // A letra "o" é convertida para "ober"
 // // A letra "u" é convertida para "ufat"
 
-function nome(){
-    var nome = window.prompt('Qual seu nome?');
-    var label = document.querySelector('label');
-    label.textContent = 'Digite uma palavra, ' +  nome + ':';
+function pegarNomeDoUsuario() {
+  let label = document.getElementById("frase");
+  let nomeUsuario = localStorage.getItem('nome')
+  
+  if (nomeUsuario == null || nomeUsuario == 'null') {
+    let user = prompt('Digite seu nome:')
+    localStorage.setItem('nome', user)
+  }
+    let userSafe = localStorage.getItem('nome')
+    label.textContent = userSafe + ", digite seu texto:"
 }
-
 
 function criptografia() {
   var caixaDeTextoEsquerdo = document.getElementById("textoEsquerdo").value;
@@ -20,7 +25,8 @@ function criptografia() {
     var popup = document.getElementById("popUp");
     popup.classList.toggle("show");
   } else {
-    var textoCriptografado = caixaDeTextoEsquerdo.toLowerCase()
+    var textoCriptografado = caixaDeTextoEsquerdo
+      .toLowerCase()
       .replace(/[^a-z ]/g, "")
       .replaceAll(/e/g, "enter")
       .replace(/i/g, "imes")
@@ -40,7 +46,8 @@ function descriptografia() {
     var popup = document.getElementById("popUp");
     popup.classList.toggle("show");
   } else {
-    var textoDescriptografado = caixaDeTextoEsquerdo.toLowerCase()
+    var textoDescriptografado = caixaDeTextoEsquerdo
+      .toLowerCase()
       .replace(/[^a-z ]/g, "")
       .replaceAll(/enter/g, "e")
       .replace(/imes/g, "i")
@@ -62,13 +69,13 @@ function copiar() {
   window.location.reload();
 }
 
-function mostrarHora(){
-    var mostrarData = document.querySelector("hr");
-    var data = new Date;
-    mostrarData.textContent = 'Hora do acesso: ' + data.getHours() + ':' + data.getMinutes();
-    mostrarData.style.fontSize = '.8rem';
-    
+function mostrarHora() {
+  var mostrarData = document.querySelector("hr");
+  var data = new Date();
+  mostrarData.textContent =
+    "Hora do acesso: " + data.getHours() + ":" + data.getMinutes();
+  mostrarData.style.fontSize = ".8rem";
 }
 
-nome()
-mostrarHora()
+mostrarHora();
+pegarNomeDoUsuario();
